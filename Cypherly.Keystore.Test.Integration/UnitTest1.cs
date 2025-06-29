43de1a2baf7a4f72cb56f655f0e8e1bdcfe76cf9
Kristian <fich2402@gmail.com>
2025-06-29 16:01:32 +0200

    added test projs

---
+++ Cypherly.Keystore.Test.Integration/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace Cypherly.Keystore.Test.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
---
+++ Cypherly.Keystore.Test.Unit/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace Cypherly.Keystore.Test.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
