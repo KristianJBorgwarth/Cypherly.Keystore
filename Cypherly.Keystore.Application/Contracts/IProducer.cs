af282d5a16bdd43cac431d6e09a4d20076412300
kristianjborgwarth <10348902@pm.me>
2025-07-13 20:54:43 +0200

    ref: move to contracts nuget for messages

--- Cypherly.Keystore.Application/Contracts/IProducer.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Cypherly.Keystore.Application.Messages;
-
-namespace Cypherly.Keystore.Application.Contracts;
-
-public interface IProducer<in TMessage> where TMessage : BaseMessage
-{
-    Task PublishMessageAsync(TMessage message, CancellationToken cancellationToken = default);
-}
